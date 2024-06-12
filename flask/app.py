c94c9d2d4556e07c934f71b757f10339f802f0c3
antonjaragon <antonio.aragon@unimi.it>
2024-06-12 10:50:08 +0200

    added env variables for quick changes

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,18 @@
 
 import flask
+import os
+import logging
+
+FINAL_DATE = os.environ.get('FINAL_DATE')
+
+logging.basicConfig(level=logging.INFO)
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    logging.info(FINAL_DATE)
+    return flask.render_template('index.html', final_date=FINAL_DATE)
 
 if __name__ == '__main__':
     app.run(debug=True)
